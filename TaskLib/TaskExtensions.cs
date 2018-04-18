63d0bf919ce258eabc6aa83bfd99bfce6e416b15
Hirad Asadi <hirad.asadi@gmail.com>
2018-04-18 09:53:24 +0200

    Updates.

---
+++ TaskLib/TaskExtensions.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskLibrary
+{
+    /// <summary>
+    ///     Extension class for tasks.
+    /// </summary>
+    public static class TaskExtensions
+    {
+        /// <summary>
+        ///     Creates a timeout task.
+        /// </summary>
+        /// <param name="timeoutTime">Timeout time</param>
+        /// <param name="handler">The timeout action</param>
+        /// <param name="tokenSource">Cancellation token source</param>        
+        public static void CreateTimeout(TimeSpan timeoutTime, Action handler,
+            CancellationTokenSource tokenSource = null)
+        {
+            if (tokenSource == null)
+                tokenSource = new CancellationTokenSource();
+
+            if (handler != null)
+                Task.Delay(timeoutTime, tokenSource.Token)
+                    .ContinueWith((task, o) => handler.Invoke(), tokenSource.Token);
+        }
+
+        /// <summary>
+        ///     This method creates a task that will asynchronously wait until any of these tasks have completed:
+        ///     - The delayed task (represents the timeout with cancellation option)
+        ///     - The main task
+        ///     If the task that was completed happens to be the main task, we cancel any cancellation tokens wait for it to
+        ///     complete and return its result.
+        ///     If the task that was completed happens to be the delayed task (a timeout has occured) we call any existing timeout
+        ///     handler
+        ///     and return default TResult.
+        /// </summary>
+        /// <typeparam name="TResult">Return type of the task</typeparam>
+        /// <param name="task">The main task</param>
+        /// <param name="timeout">Timeout value</param>
+        /// <param name="timeoutHandler">Timeout handler</param>
+        /// <param name="cancellationTokenSourceAction">Cancellation token source</param>
+        /// <returns>TResult (default value for TResult if timeout has occured)</returns>
+        public static async Task<TResult> TimeoutAfter<TResult>(this Task<TResult> task,
+            TimeSpan timeout,
+            Action timeoutHandler = null,
+            CancellationTokenSource cancellationTokenSourceAction = null)
+        {
+            var timeoutCancellationTokenSource = cancellationTokenSourceAction ?? new CancellationTokenSource();
+
+            using (timeoutCancellationTokenSource)
+            {
+                var delayedTask = Task.Delay(timeout, timeoutCancellationTokenSource.Token);
+
+                if (await Task.WhenAny(task, delayedTask) == task)
+                {
+                    timeoutCancellationTokenSource.Cancel();
+                    return await task; // For propagating exceptions
+                }
+
+                // Timeout occured, invoke handler if there is any
+                if (timeoutHandler != null && !timeoutCancellationTokenSource.IsCancellationRequested)
+                    await Task.Run(() => timeoutHandler.Invoke(), timeoutCancellationTokenSource.Token);
+
+                return default;
+            }
+        }
+
+        /// <summary>
+        ///     This method creates a task that will asynchronously wait until any of these tasks have completed:
+        ///     - The delayed task (represents the timeout with cancellation option)
+        ///     - The main task
+        ///     If the task that was completed happens to be the main task, we cancel any cancellation tokens wait for it to
+        ///     complete and return its result.
+        ///     If the task that was completed happens to be the delayed task (a timeout has occured) we call any existing timeout
+        ///     handler.
+        /// </summary>
+        /// <param name="task">The main task</param>
+        /// <param name="timeout">Timeout value</param>
+        /// <param name="timeoutHandler">Timeout handler</param>
+        /// <param name="cancellationTokenSourceAction">Cancellation token source</param>
+        public static async Task TimeoutAfter(this Task task,
+            TimeSpan timeout,
+            Action timeoutHandler = null,
+            CancellationTokenSource cancellationTokenSourceAction = null)
+        {
+            var timeoutCancellationTokenSource = cancellationTokenSourceAction ?? new CancellationTokenSource();
+
+            using (timeoutCancellationTokenSource)
+            {
+                var delayedTask = Task.Delay(timeout, timeoutCancellationTokenSource.Token);
+
+                if (await Task.WhenAny(task, delayedTask) == task)
+                {
+                    timeoutCancellationTokenSource.Cancel();
+                    await task; // For propagating exceptions
+                }
+                else
+                {
+                    // Timeout occured, invoke handler if there is any
+                    if (timeoutHandler != null && !timeoutCancellationTokenSource.IsCancellationRequested)
+                        await Task.Run(() => timeoutHandler.Invoke(), timeoutCancellationTokenSource.Token);
+                }
+            }
+        }
+    }
+}
---
+++ TaskTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TaskLibrary;
+using TaskExtensions = TaskLibrary.TaskExtensions;
+
+namespace TaskTest
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            // Main task
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Main task running...");
+                
+                Thread.Sleep(10000); // Sleep for 10 seconds
+                
+            })
+            .TimeoutAfter(TimeSpan.FromSeconds(5), () => TimeoutTask(1)); // Timeout after 5 seconds (no cancellation token)
+
+            // Or a general timeout not bound to an external task (no cancellation token)
+            TaskExtensions.CreateTimeout(TimeSpan.FromSeconds(5), () => TimeoutTask(2));
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        ///     Timeout task.
+        /// </summary>
+        private static void TimeoutTask(int i)
+        {
+            Console.WriteLine($"TIMEOUT {i} OCCURED!");
+        }
+    }
+}
